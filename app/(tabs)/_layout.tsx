a718107bf804e824f7ae15a0f8e1b95345c6da41
shazab-saifi <shazebsaifi92@gmail.com>
2026-02-02 16:24:35 +0530

    Created tabs

---
+++ app/(tabs)/_layout.tsx
@@ -0,0 +1,44 @@
+import { Ionicons } from "@expo/vector-icons";
+import { Tabs } from "expo-router";
+
+export default function RootLayout() {
+  return (
+    <Tabs
+      screenOptions={{
+        tabBarActiveTintColor: "#14b8a6",
+        tabBarStyle: {
+          backgroundColor: "#262626",
+          borderColor: "#525252",
+        },
+      }}
+    >
+      <Tabs.Screen
+        name="index"
+        options={{
+          headerShown: false,
+          tabBarIcon: ({ color }) => (
+            <Ionicons name="home-outline" size={24} color={color} />
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="search"
+        options={{
+          headerShown: false,
+          tabBarIcon: ({ color }) => (
+            <Ionicons name="search" size={24} color={color} />
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="watchlist"
+        options={{
+          headerShown: false,
+          tabBarIcon: ({ color }) => (
+            <Ionicons name="list" size={24} color={color} />
+          ),
+        }}
+      />
+    </Tabs>
+  );
+}
